4276c0f9dfe82a91ad4602437ccba592fbaa919b
Ramsey Chapin <ramsey@ramseychapin.com>
2019-06-02 19:52:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,12 @@ class Song
 
   def self.artist_count
     artist_count = {}
-    @@artists.each {|artist| artist_count[artist] += 1}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
     artist_count
   end
 
