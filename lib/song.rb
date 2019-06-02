10199def4f622595fda55ac1222f729a5c517b01
Ramsey Chapin <ramsey@ramseychapin.com>
2019-06-02 19:53:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,12 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each {|genre| genre_count[genre] += 1}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
     genre_count
   end
 
