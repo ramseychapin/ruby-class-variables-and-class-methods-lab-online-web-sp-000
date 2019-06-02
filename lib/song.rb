fabc28d0289d85e537d5e8dd50b12a7c562d32b8
Ramsey Chapin <ramsey@ramseychapin.com>
2019-06-02 19:33:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 class Song
-
+  @@song_number = 0
   attn_reader :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-
   end
+
+
 end
